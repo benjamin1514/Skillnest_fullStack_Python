0ea50e7267b408b9a6a20408face6b18e5917afe
benjamin1514 <benjamindelgado@liceovvh.cl>
2026-04-15 11:33:50 -0400

    .

--- python/fundamentos/extras/fundamentos/03_bucles_for_basico_core_benjamin_2026-04-14/01_bucle_for_basico.py
+++ python/fundamentos/extras/fundamentos/03_bucles_for_basico_core_benjamin_2026-04-14/01_bucle_for_basico.py
@@ -20,19 +20,38 @@ usando ejemplos inspirados en videojuegos y situaciones atractivas.
 # ¡Cuidado con la prioridad en tus condicionales!
 # (Tu código aquí)
 
+def trampaEmoji():
+    for i in range(1, 100):
+        if i % 5 == 0:
+            print("Es divisible por 5")
+        elif i % 10 == 0:
+            print("Es divisible por 10")
+        else:
+            print("No es un número")
 
 # 4. Suma colosal
 # Suma todos los números pares del 0 al 500,000 e imprime la suma total.
 # (Tu código aquí)
 
+def sumaPares():
+    sumaPares = 0
+    limite = 500000
+    
+    for i in range(1, limite + 1):
+        if i % 2 == 0:
+            sumaPares += i
+    print(f"La suma de los números pares hasta {limite} es: {sumaPares}")
+
+sumaPares()
 
 # 5. Retroceso temporal
 # Desde 2024, retrocede de 3 en 3 hasta 0 o menos.
 # Imprime cada valor en la cuenta regresiva.
 # (Tu código aquí)
 
-for i in range(2024, 0, -3):
-    print(f"Año {i}")
+def retrocesoTemporal():
+    for i in range(2024, 0, -3):
+        print(f"Año {i}")
 
 
 # 6. Contador dinámico
