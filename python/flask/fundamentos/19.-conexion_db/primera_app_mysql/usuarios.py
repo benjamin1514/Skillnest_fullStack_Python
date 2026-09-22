d8a05cdc26de32548e2c038632320f6f78b7687e
benjamin1514 <benjamindelgado@liceovvh.cl>
2026-09-22 11:39:23 -0300

    .

---
+++ python/flask/fundamentos/19.-conexion_db/primera_app_mysql/usuarios.py
@@ -0,0 +1,19 @@
+from mysqlconnection import connectToMySQL
+
+class Usuarios:
+    def __init__(self, data):
+        self.id = data["id"]
+        self.nombre = data["nombre"]
+        self.correo = data["correo"]
+        self.edad = data["edad"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+        
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM usuarios;"
+        results = connectToMySQL("primera_flask").query_db(query)
+        usuarios = []
+        for row in results:
+            usuarios.append(cls(row))
+        return usuarios
